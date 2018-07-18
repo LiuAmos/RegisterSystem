55bb756cc83da93e931c702597723a6d0acfe8a6
LiuAmos <b10332011@gmail.com>
2018-07-18 16:28:19 +0800

    0718lab

---
+++ login.php
@@ -0,0 +1,50 @@
+
+<?php
+	session_start();
+
+	$_SESSION['account'];
+	$_SESSION['password'];
+?>
+
+
+
+<html>
+
+<head>
+<title>Login</title>
+</head>
+
+<body>
+
+	<?php
+
+	//echo $_SESSION['account'];
+	//echo '<br>';  
+ 	//echo $_SESSION['password'];
+
+ 	?> 
+
+	<form action="" method="get">
+  	account   : <input type="text" name="account"><br>
+  	password  : <input type="text" name="password"><br>
+  	<input type="submit" value="登入" name="submit">
+	</form>
+
+<?php
+
+	
+
+	if (isset($_GET['submit'])) { 
+	$_SESSION['account'] = $_GET['account'];
+	$_SESSION['password'] = $_GET['password'];
+
+	//echo($_SESSION['account']);
+	//echo '<br>'; 
+ 	//echo($_SESSION['password']);
+ 	} 
+?> 
+
+</body>
+
+
+</html>
---
+++ register.php
@@ -0,0 +1,28 @@
+<!Doctype html>
+
+<html>
+
+<head>
+<title>Register</title>
+</head>
+
+<body>
+
+	<form action="demo_form.php" method="get">
+  	account   : <input type="text" name="account"><br>
+  	password  : <input type="text" name="password"><br>
+  	name      : <input type="text" name="name"><br>
+  	phone     : <input type="text" name="phone"><br>
+  	email     : <input type="text" name="email"><br>
+  	birthday  : <input type="text" name="birthday"><br>
+  	skype     : <input type="text" name="skype"><br>
+  	graduation: <input type="text" name="graduation"><br>
+  	<input type="submit" value="註冊">
+	</form>
+
+
+
+</body>
+
+
+</html>

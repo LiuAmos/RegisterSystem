356d95ca457bf426393978f6ad14dc678415a229
LiuAmos <b10332011@gmail.com>
2018-07-19 22:42:39 +0800

    0719home

--- login.php
+++ login.php
@@ -1,9 +1,8 @@
 
 <?php
-	session_start();
-
-	$_SESSION['account']="";
-	$_SESSION['password']="";
+session_start();
+$_SESSION['account']="";
+$_SESSION['password']="";
 ?>
 
 
@@ -17,83 +16,55 @@
 <body>
 
 	<?php
-
 	//debug
 	//echo $_SESSION['account'];
 	//echo '<br>';  
  	//echo $_SESSION['password'];
+	?> 
 
- 	?> 
+
 
 	<form action="membership.php" method="get">
-  	account   : <input type="text" name="account"><br>
-  	password  : <input type="text" name="password"><br>
-  	<input type="submit" value="登入" name="submit" >
+		account   : <input type="text" name="account"><br>
+		password  : <input type="text" name="password"><br>
+		<input type="submit" value="登入" name="submit" >
 	</form>
 
+
+
+	<?php
+	//註冊資料進資料庫
+
+	if (isset($_GET['submit1'])) {
+
+ 		$reg_account=$_GET['account'];
+ 		$reg_password=$_GET['password'];
+ 		$reg_name=$_GET['name'];
+ 		$reg_phone=$_GET['phone'];
+ 		$reg_email=$_GET['email'];
+ 		$reg_birthday=$_GET['birthday'];
+ 		$reg_skype=$_GET['skype'];
+ 		$reg_graduation=$_GET['graduation'];
+
+
+		//debug
+		//echo $reg_account;
+		//echo $reg_password;
+
+		//建立連線
+  		$db_link = mysqli_connect('localhost','root','','registersystem') or die("could not connect".mysqli_error());
+  		mysqli_set_charset($db_link,"utf8");
+
+		//操作資料庫
  
+ 		mysqli_select_db($db_link,"registersystem");
+		mysqli_query($db_link,"INSERT INTO student (account,password,name,phone,email,birthday,skype,graduation) VALUES ('$reg_account','$reg_password','$reg_name','$reg_phone','$reg_email','$reg_birthday','$reg_skype','$reg_graduation')");
 
+ 		mysqli_close($db_link);
 
+	}
+	?>
 
- <?php
-//註冊資料進資料庫
-
-  if (isset($_GET['submit1'])) {
-
- $reg_account=$_GET['account'];
- $reg_password=$_GET['password'];
- $reg_name=$_GET['name'];
- $reg_phone=$_GET['phone'];
- $reg_email=$_GET['email'];
- $reg_birthday=$_GET['birthday'];
- $reg_skype=$_GET['skype'];
- $reg_graduation=$_GET['graduation'];
-
-
-//debug
-//echo $reg_account;
-//echo $reg_password;
-
-//建立連線
-  $db_link = mysqli_connect('localhost','root','','registersystem') or die("could not connect".mysqli_error());
-  mysqli_set_charset($db_link,"utf8");
-
-//操作資料庫
- 
- mysqli_select_db($db_link,"registersystem");
-mysqli_query($db_link,"INSERT INTO student (account,password,name,phone,email,birthday,skype,graduation) 
-VALUES ('$reg_account','$reg_password','$reg_name','$reg_phone','$reg_email','$reg_birthday','$reg_skype','$reg_graduation')");
-
- mysqli_close($db_link);
-
-}
- ?>
-
-
- <?php	
-/*
-	if (isset($_GET['submit'])) { 
-	$_SESSION['account'] = $_GET['account'];
-	$_SESSION['password'] = $_GET['password'];
-	//debug
-	//echo $_SESSION['account'];
-	//echo '<br>'; 
- 	//echo $_SESSION['password'];
-
-//建立連線
- $db_link = mysqli_connect('localhost','root','','registersystem') or die("could not connect".mysqli_error());
- mysqli_set_charset($db_link,"utf8");
-
-//操作資料庫
-mysqli_select_db($db_link,"registersystem");
-$result= mysqli_query($db_link, "select account,password from 'student'");
-$row = mysqli_fetch_row($result);
-echo "&result[0]";
-
-
-
- 	} */
-?> 
 
 </body>
 
--- membership.php
+++ membership.php
@@ -8,75 +8,73 @@
 
 <body>
 
-<?php
+  <?php
 
-//驗證帳密  
-
+  //驗證帳密  
   if (isset($_GET['submit'])) { 
 
-  $_SESSION['account'] = $_GET['account'];
-  $_SESSION['password'] = $_GET['password'];
-	//debug
-	//echo $_SESSION['account'];
-	//echo '<br>';  
- 	//echo $_SESSION['password'];
+    $_SESSION['account'] = $_GET['account'];
+    $_SESSION['password'] = $_GET['password'];
+    $session_account=$_GET['account'];//用來辨識去資料庫撈哪組帳號
+    //debug
+    //echo $_SESSION['account'];
+    //echo '<br>';  
+ 	  //echo $_SESSION['password'];
 
-  //建立連線
- $db_link = mysqli_connect('localhost','root','','registersystem') or die("could not connect".mysqli_error());
- mysqli_set_charset($db_link,"utf8");
+    //建立連線
+    $db_link = mysqli_connect('localhost','root','','registersystem') or die("could not connect".mysqli_error());
+    mysqli_set_charset($db_link,"utf8");
 
-//操作資料庫
-mysqli_select_db($db_link,"registersystem");
-$result= mysqli_query($db_link, "SELECT account,password FROM student WHERE account ='b12345' ");
-$row = mysqli_fetch_row($result);//變成array
+    //操作資料庫
+    mysqli_select_db($db_link,"registersystem");
+    $result= mysqli_query($db_link, "SELECT account,password FROM student WHERE account = '$session_account' ");
+    $row = mysqli_fetch_row($result);//變成array
 
-//debug
-//echo $row[0];
-//echo $row[1];
-if($_SESSION['account']==$row[0] AND  $_SESSION['password']==$row[1]){
-echo "login success";
-echo  "<table style='width:100%'>
+    //debug
+    //echo $row[0];
+    //echo $row[1];
 
-  <tr>
-    <th>Account</th>
-    <th>Password</th> 
-    <th>Name</th>
-    <th>Phone</th>
-    <th>Email</th>
-    <th>Birthday</th>
-    <th>Skype</th>
-    <th>Graduation</th>
-  </tr>
-
-  <tr>
-    <td>2</td>
-    <td>2</td> 
-    <td>2</td>
-    <td>2</td>
-    <td>2</td>
-    <td>2</td>
-    <td>2</td>
-    <td>2</td>
-  </tr>
-
-   <tr>
-   <td>2</td>
-   <td>2</td> 
-   <td>2</td>
-   <td>2</td>
-   <td>2</td>
-   <td>2</td>
-   <td>2</td>
-   <td>2</td>
-   </tr>
-
-</table>";
+    if($_SESSION['account']==$row[0] AND  $_SESSION['password']==$row[1]){
+      echo "login success";
+      $result1= mysqli_query($db_link, "SELECT * FROM student WHERE account = '$session_account' ");
+      $row1 = mysqli_fetch_row($result1);//變成array
 
 
-}
-else{echo "false";} 
-}
-?> 
+      echo  "<table style='width:100%'>
+
+      <tr>
+      <th>Account</th>
+      <th>Password</th> 
+      <th>Name</th>
+      <th>Phone</th>
+      <th>Email</th>
+      <th>Birthday</th>
+      <th>Skype</th>
+      <th>Graduation</th>
+      </tr>
+
+      <tr>
+      <td>$row1[0]</td>
+      <td>$row1[1]</td> 
+      <td>$row1[2]</td>
+      <td>$row1[3]</td>
+      <td>$row1[4]</td>
+      <td>$row1[5]</td>
+      <td>$row1[6]</td>
+      <td>$row1[7]</td>
+      </tr>
+
+      </table>";
+
+      echo "<input type ='submit'  onclick='history.back()' value='登出'></input>";
+    }
+    else{echo "false";}
+
+
+
+    mysqli_close($db_link);
+  }
+  ?> 
 
 
 
